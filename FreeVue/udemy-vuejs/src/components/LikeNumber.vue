88293821411ea793213a20995b2a4796f776839b
ぜん <zen@mac.local>
2020-11-25 09:47:39 +0900

    component
    vue cli component

---
+++ FreeVue/udemy-vuejs/src/components/LikeNumber.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>いいね({{ number }})</p>
+    <button @click="increment">+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      number: 3
+    };
+  },
+  methods: {
+    increment: function () {
+      this.number += 1;
+    }
+  }
+};
+
+</script>
+
+<style>
+
+</style>
